a523bbfb75260ca596161b2f023033d2e890c20b
GemmaClaveroDelMoral <casagemmayraul@yahoo.es>
2024-02-25 20:41:29 -0500

    buscar palabra mas larga de array con forEach

--- 02.js
+++ 02.js
@@ -6,7 +6,7 @@
 */
 function stringMasLarga(strings) {
   // La función llamada 'stringMasLarga', recibe como argumento un arreglo de strings llamado 'strings'
-  // y debe devolver el string más largo que hay en el arreglo (Es decir el de mayor cantidad de caracteres)
+  // y debe devolver string más largo que hay en el arreglo (Es decir el de mayor cantidad de caracteres)
   // Ej:
   // stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); debe retornar 'guten tag'
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
@@ -19,5 +19,5 @@ function stringMasLarga(strings) {
 }
 
 // No modifiques nada debajo de esta linea //
-console.log(stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']))
+console.log(stringMasLarga(['hi', 'hello', 'ni hao', 'guten tagisimo']))
 module.exports = stringMasLarga
